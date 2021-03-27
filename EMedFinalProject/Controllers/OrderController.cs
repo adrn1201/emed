a946f2542809cc64d5e26787ca8b4c98e374a572
Carlos Adrian Catindig <11920236@benilde.edu.ph>
2021-03-27 22:28:24 +0800

    Added dynamic field codes in OrderProcess Controller

--- EMedFinalProject/Controllers/OrderController.cs
+++ EMedFinalProject/Controllers/OrderController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using EMedFinalProject.Data;
 using EMedFinalProject.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace EMedFinalProject.Controllers
 {
@@ -26,31 +28,70 @@ namespace EMedFinalProject.Controllers
         {
             return View();
         }
+
+        public IActionResult MercuryOrderProcess()
+        {
+            return View();
+        }
+
         public IActionResult OrderProcess()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult OrderProcess(OrderViewModel record)
+        public async Task<IActionResult> OrderProcess(OrderViewModel record) //IFormFile file)
         {
-            //StartWizard Step 1
-            var order = new OrderViewModel();
-            {
-                order.LastName = record.LastName;
-                order.FirstName = record.FirstName;
-                order.DeliveryAddress = record.DeliveryAddress;
-                order.ContactNo = record.ContactNo;
-                order.Email = record.Email;
-                //order.Prescription = record.Prescription;
-                //order.ValidID = record.ValidID;
-                order.BranchID = record.BranchID;//temporary placement
-                order.PaymentMethod = record.PaymentMethod;//temporary placement
-                   
-            }
+            //if (ModelState.IsValid)
+            //{
+            //    if (file != null)
+            //    {
+            //        if (file.Length > 0)
+            //        {
+            //            //Getting FileName
+            //            var fileName = Path.GetFileName(file.FileName);
+            //            //Getting file Extension
+            //            var fileExtension = Path.GetExtension(fileName);
+            //            // concatenating  FileName + FileExtension
+            //            var newFileName = String.Concat(Convert.ToString(Guid.NewGuid()), fileExtension);
 
-            /*Next...*/
-            return View();
+            //            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/services", newFileName);
+            //            record.Prescription = newFileName;
+
+            //            using var stream = new FileStream(filePath, FileMode.Create);
+            //            file.CopyTo(stream);
+            //        }
+            //    }
+                var branches = await _context.Branches.Where(b => b.BranchId == record.BranchID).SingleOrDefaultAsync();
+
+                var order = new OrderViewModel();
+                {
+                    order.LastName = record.LastName;
+                    order.FirstName = record.FirstName;
+                    order.DeliveryAddress = record.DeliveryAddress;
+                    order.ContactNo = record.ContactNo;
+                    order.Email = record.Email;
+                    order.Branch = branches;
+                    order.BranchID = record.BranchID;
+                    order.PaymentMethod = record.PaymentMethod;
+
+                }
+
+                //Dynamic Fields
+                foreach (var productOrders in record.OrderList)
+                {
+                    var products = new OrderDetailsViewModel()
+                    {
+                        ProductName = productOrders.ProductName,
+                        Milligrams = productOrders.Milligrams,
+                        Quantity = productOrders.Quantity,
+                        Instructions = productOrders.Instructions
+
+                    };
+                }
+
+                return RedirectToAction("OrderForm", "Account", new { success = "yes" });
+            }
         }
     }
-}
+
